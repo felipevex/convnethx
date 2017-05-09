0e65a0e753cd0ceac43f6b272ace4dd328177a02
Felipe Peternella <felipevex@gmail.com>
2017-05-09 10:21:05 -0300

    Opt model

---
+++ convnethx/Opt.hx
@@ -0,0 +1,14 @@
+package convnethx;
+
+typedef Opt = {
+    @:optional var sx:Int;
+    @:optional var out_sx:Int;
+    @:optional var width:Int;
+
+    @:optional var sy:Int;
+    @:optional var out_sy:Int;
+    @:optional var height:Int;
+
+    @:optional var depth:Int;
+    @:optional var out_depth:Int;
+}
